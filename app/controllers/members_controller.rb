b7d41b63f86e34b558d7826ab2f574d3716289bd
Ernesto Araiza <yo@ernestoaraiza.com>
2016-09-26 22:17:26 -0500

    creating and showing one record

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,8 +1,20 @@
 class MembersController < ApplicationController
+	def show
+		@member = Member.find(params[:id])
+	end
+	
 	def new
 	end
 
-        def create
-          render plain: params[:member].inspect
-        end
+  def create
+    @member = Member.new(member_params)
+
+    @member.save
+    redirect_to @member
+  end
+
+  private
+  	def member_params
+  		params.require(:member).permit(:nombre, :apellido, :index, :fecha_inicio, :fecha_fin, :tipo_contrato)
+  	end
 end
