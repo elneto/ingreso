35bfcba09283bd691d01c1a9052958cef83eb8f8
neto ubuntu <yo@ernestoaraiza.com>
2016-09-29 15:35:12 -0500

    starting to check validations

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,20 +2,24 @@ class MembersController < ApplicationController
 	def index
 		@members = Member.all
 	end
-	
+
 	def show
 		@member = Member.find(params[:id])
 		#TODO send email to all approvers about this new request
 	end
 	
 	def new
+		@member = Member.new
 	end
 
   def create
     @member = Member.new(member_params)
 
-    @member.save
-    redirect_to @member
+    if @member.save
+		redirect_to @member
+	else
+		render 'new'
+	end
   end
 
   private
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,9 @@
 class Member < ApplicationRecord
+	include ActiveModel::Validations
+
+	attr_accessor :nombre, :appellido
+
+	validates :nombre, presence: true, length: {minimum: 2}
+	validates :apellido, presence: true, length: {minimum: 2}
+					   
 end
