42d7e5931c1fca7d9fa997346c774eb0c811f82f
neto ubuntu <yo@ernestoaraiza.com>
2016-09-30 12:08:56 -0500

    adjusted datepicker to format dd/mm/yy

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	$('#member_fecha_inicio').datepicker();
-	$('#member_fecha_fin').datepicker();
+	$('#member_fecha_inicio').datepicker({dateFormat: 'dd/mm/yy'});
+	$('#member_fecha_fin').datepicker({dateFormat: 'dd/mm/yy'});
 });
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,6 @@
 class Member < ApplicationRecord
 	include ActiveModel::Validations
 
-	attr_accessor :nombre, :appellido
-
 	validates :nombre, :apellido, presence: { message: "no puede estar vacío"}, length: {minimum: 2, message: "es muy corto (mínimo 2 letras)"}
 	validates :fecha_inicio, :fecha_fin, :tipo_contrato, :index, presence: { message: "no puede estar vacío"}
 	validates :index, numericality: { only_integer: true, message: "debe tener sólo números" }
