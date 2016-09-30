1931d1ea7dadf6b0f14ed6bdc65b666c4cc0de8e
neto ubuntu <yo@ernestoaraiza.com>
2016-09-30 14:31:31 -0500

    fixed bug that prevented datepicker to work on link

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
 	$('#member_fecha_inicio').datepicker({dateFormat: 'dd/mm/yy'});
 	$('#member_fecha_fin').datepicker({dateFormat: 'dd/mm/yy'});
 });
